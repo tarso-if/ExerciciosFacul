12a42ec227ffcd2313cc7884f8399a411c7ac46c
tarso-if <tarso.silva@aluno.farroupilha.ifrs.edu.br>
2026-08-26 15:23:03 -0300

    Update CalcularTaxaCambio.java

--- CalcularTaxaCambio.java
+++ CalcularTaxaCambio.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class CalcularTaxaCambio{
 
     // Calcula a taxa de cambio
     public static double valorConvertido(double taxa, double real) {
